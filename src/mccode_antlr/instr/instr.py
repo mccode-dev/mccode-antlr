dae10d876158d1363f18eaa608abd47113f2592a
Gregory S. Tucker <gregory.tucker@ess.eu>
2026-08-26 15:31:59 +0200

    Fix error from GETPATH(data); functionality likely remains broken

--- src/mccode_antlr/instr/instr.py
+++ src/mccode_antlr/instr/instr.py
@@ -682,8 +682,10 @@ class Instr(Struct):
     def _getpath(self, filename: str):
         from pathlib import Path
         for registry in self.registries:
-            if registry.known(filename):
+            if registry.known(filename, strict=True):
+                print(registry.path(filename))
                 return registry.path(filename).absolute().resolve()
+        logger.warning(f'Unable to resolve path of "{filename}" in any of\n\t{"\t".join(str(r) for r in self.registries)}')
         return Path()
 
     def _replace_env_getpath_cmd(self, flags: str):
