7786fd31efde941a16e028f6a616f5b8284c0b50
Gregory S. Tucker <gregory.tucker@ess.eu>
2026-09-04 07:48:08 +0000

    [Ref] adopt 'COPY(instance) never copies EXTEND' presumptive grammar rule (#332)

--- src/mccode_antlr/instr/instance.py
+++ src/mccode_antlr/instr/instance.py
@@ -110,12 +110,17 @@ class Instance(Struct):
 
     @classmethod
     def from_instance(cls, name: str, ref: InstanceReference, at: VectorReference, rotate: AnglesReference):
-        # from copy import deepcopy
         # copy each of: parameters, extend, group, jump, when, metadata
+        #
+        # A bug in upstream McCode's code generator always replaces a copied EXTEND
+        # by an empty block: https://github.com/mccode-dev/McCode/issues/2621
+        # This bug is being promoted to a feature.
+        # If the upstream behavior changes again, the commented extend line below
+        # can be restored.
         return cls(name, ref.type, at, rotate,
                    parameters=tuple([par for par in ref.parameters]),
                    when=ref.when, group=ref.group,
-                   extend=tuple([ext for ext in ref.extend]),
+                   #extend=tuple([ext for ext in ref.extend]),
                    jump=tuple([jmp for jmp in ref.jump]),
                    metadata=tuple([md for md in ref.metadata]))
 
