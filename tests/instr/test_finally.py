a97e00d173d98cd4d9b2908065d357e43f20719c
Gregory Tucker <gregory.tucker@gmail.com>
2026-09-05 17:20:15 +0200

    [Ref] Actually build and run an instrument for the FINALLY test

--- tests/instr/test_finally.py
+++ tests/instr/test_finally.py
@@ -1,17 +1,18 @@
 from unittest import TestCase
-
+from mccode_antlr.test import compiled_test
 
 class TestInstrFinally(TestCase):
+    @compiled_test
     def test_parse_finally(self):
         from mccode_antlr.instr import Instr
-        from mccode_antlr.common import InstrumentParameter, Expr, DataType
         from mccode_antlr.loader import parse_mcstas_instr
+        from mccode_antlr.utils import compile_and_run
         instr_source = """
         DEFINE INSTRUMENT test_finally_parse(par0=3.14159)
         TRACE
         FINALLY
         %{
-        printf("this finally block is parsed\n");
+        printf("this finally block is parsed\\n");
         %}
         END
         """
@@ -23,13 +24,16 @@ class TestInstrFinally(TestCase):
         self.assertEqual(len(instr.final), 1)
         self.assertTrue("this finally block is parsed" in str(instr.final[0]))
 
+        std_output, dats = compile_and_run(instr, "-n 1 -y")
+        self.assertTrue("this finally block is parsed" in std_output.decode())
+
+
     def test_assemble_empty_trace(self):
         from mccode_antlr.instr import Instr
-        from mccode_antlr.common import InstrumentParameter, Expr, DataType
         from mccode_antlr.utils import make_assembler
 
         assembler = make_assembler('test_finally_assemble')
-        assembler.final('printf("this finally block is present\n");')
+        assembler.final('printf("this finally block is present\\n");')
 
         instr = assembler.instrument
         self.assertTrue(isinstance(instr, Instr))
